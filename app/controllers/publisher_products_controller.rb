e36340a884e69a3f466aaa750b4bde917e758ec5
ovinix <dev.shvvolodymyr@hotmail.com>
2015-07-07 19:51:30 +0300

    Products List Load more Done

--- app/controllers/publisher_products_controller.rb
+++ app/controllers/publisher_products_controller.rb
@@ -52,8 +52,22 @@ class PublisherProductsController < ApplicationController
     
     # @publisher_products = PublisherProduct.where("publisher_id = ?", @publisher_id).order(sort_column + " " + sort_direction) # .paginate(:per_page => 200, :page => params[:page])
     
-    @publisher_products = current_user.publisher.publisher_products.order(sort_column + " " + sort_direction)
-    
+    # @publisher_products = current_user.publisher.publisher_products.order(sort_column + " " + sort_direction)
+    # @publisher_products = current_user.publisher.publisher_products.order(sort_column + " " + sort_direction).limit(2)
+    @itemsShowed = 0
+
+    # if the id params is present
+    if params[:id]
+      # get all records with id less than 'our last id'
+      # and limit the results to 5
+      @publisher_products = current_user.publisher.publisher_products.order(sort_column + " " + sort_direction).where('id < ?', params[:id]).limit(2)
+    else
+      @publisher_products = current_user.publisher.publisher_products.order(sort_column + " " + sort_direction).limit(2)
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
     
   end
   
