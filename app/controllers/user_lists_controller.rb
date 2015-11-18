7618a63f6eaa22ad5d72a87e83f4a216aaa470c7
ovinix <dev.shvvolodymyr@hotmail.com>
2015-11-18 21:48:18 +0200

    Search with options (Age field not working)

--- app/controllers/user_lists_controller.rb
+++ app/controllers/user_lists_controller.rb
@@ -14,9 +14,28 @@ class UserListsController < ApplicationController
 
 	    # Search in database
 	    if search_query.present?
-	    	@search_results = User.where("name_first ilike :sq or name_last ilike :sq or name_first || ' ' || name_last ilike :sq", sq: "%#{search_query}%").paginate(page: params[:page], per_page: 15)
-	    end	
-	    
+	    	# @search_results = User.where("name_first ilike :sq or name_last ilike :sq or name_first || ' ' || name_last ilike :sq", sq: "%#{search_query}%").paginate(page: params[:page], per_page: 15)
+	    	temp_results = User.where("name_first ilike :sq or name_last ilike :sq or name_first || ' ' || name_last ilike :sq", sq: "%#{search_query}%")
+
+	    	if params[:search_gender].present?
+	    		temp_results = temp_results.where(:gender => params[:search_gender].to_i)
+	    	end
+
+	    	if params[:search_bd_year].present?
+	    		temp_results = temp_results.where(:bd_year => params[:search_bd_year].to_i)
+	    	end
+
+	    	if params[:search_bd_month].present?
+	    		temp_results = temp_results.where(:bd_month => params[:search_bd_month].to_i)
+	    	end
+
+	    	if params[:search_bd_day].present?
+	    		temp_results = temp_results.where(:bd_day => params[:search_bd_day].to_i)
+	    	end
+
+	    	@search_results = temp_results.paginate(page: params[:page], per_page: 15)
+	    end
+
 	end
 
 end
