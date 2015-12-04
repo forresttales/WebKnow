48175e71b375d25d1866817701b5a79bbfefce8d
ovinix <dev.shvvolodymyr@hotmail.com>
2015-12-04 16:51:39 +0200

    Content Types, File Types, Enchencments, Literacy Standarts Filters Added

--- app/controllers/publisher_product_lists_controller.rb
+++ app/controllers/publisher_product_lists_controller.rb
@@ -44,6 +44,42 @@ class PublisherProductListsController < ApplicationController
 			temp_results = temp_results.joins(:publisher_product_platform).where(search_or_platforms_query)
 		end
 
+		if params[:search_content_types].present?
+			search_or_content_types_query = ""
+			params[:search_content_types].each do |content_type_id|
+				search_or_content_types_query += " or " if search_or_content_types_query.present?
+				search_or_content_types_query += "publisher_product_content_types.content_type_" + content_type_id + " = true"
+			end
+			temp_results = temp_results.joins(:publisher_product_content_type).where(search_or_content_types_query)
+		end
+
+		if params[:search_file_types].present?
+			search_or_file_types_query = ""
+			params[:search_file_types].each do |file_type_id|
+				search_or_file_types_query += " or " if search_or_file_types_query.present?
+				search_or_file_types_query += "publisher_product_file_types.file_type_" + file_type_id + " = true"
+			end
+			temp_results = temp_results.joins(:publisher_product_file_type).where(search_or_file_types_query)
+		end
+
+		if params[:search_enhancements].present?
+			search_or_enhancements_query = ""
+			params[:search_enhancements].each do |enhancement_id|
+				search_or_enhancements_query += " or " if search_or_enhancements_query.present?
+				search_or_enhancements_query += "publisher_product_enhancements.enhancement_" + enhancement_id + " = true"
+			end
+			temp_results = temp_results.joins(:publisher_product_enhancement).where(search_or_enhancements_query)
+		end
+
+		if params[:search_core_literacy_standards].present?
+			search_or_cls_query = ""
+			params[:search_core_literacy_standards].each do |cls_id|
+				search_or_cls_query += " or " if search_or_cls_query.present?
+				search_or_cls_query += "publisher_product_core_literacy_standards.id_standard" + " = " + cls_id
+			end
+			temp_results = temp_results.joins(:publisher_product_core_literacy_standards).where(search_or_cls_query).group("publisher_products.id")
+		end
+
 		if params[:search_from_age].present?
 			temp_results = temp_results.joins(:publisher_product_age_range).where("publisher_product_age_ranges.age_from >= :sfa", sfa: "#{params[:search_from_age].to_i}")
 		end
